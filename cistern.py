ff558dc7bee115df989c25908788e84a4d95d445
Michael Hancock <michaelhancock89@gmail.com>
2016-08-17 00:46:43 -0700

    Hello World\!

---
+++ cistern.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Python built-in modules
+import os
+import sys
+
+# Installed from PyPI
+import click
+from configobj import ConfigObj
+import feedparser
+from peewee import *
+from validate import Validator
+
+
+click.echo("Hello World!")
